0a27e6f8d0efa61714b3986ee784f2ba6927d2a0
medinatrum <84572295+medinatrum@users.noreply.github.com>
2021-09-11 12:35:48 +0200

    main.tf added.

---
+++ s3-backend/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+ region = "us-east-2"
+ access_key = ""                   
+ secret_key = ""                   
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+ bucket = var.bucket_name
+
+ force_destroy = true
+
+ versioning {
+  enabled = true
+ }
+
+ server_side_encryption_configuration {
+  rule {
+   apply_server_side_encryption_by_default {
+   sse_algorithm = "AES256"
+   }
+  }
+ }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+ name = var.table_name   
+ billing_mode = "PAY_PER_REQUEST"
+ hash_key = "LockID"
+
+ attribute {
+  name = "LockID"
+  type = "S"
+ }
+}
+
+terraform {
+ backend "s3" {
+ bucket = "diplomski-rad-terraform-state"                          
+ key = "global/s3/terraform.tfstate"
+ region = "us-east-2"
+
+ dynamodb_table = "terraform-locks"
+  encrypt = true
+ }
+}
