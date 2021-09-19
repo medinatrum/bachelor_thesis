650ba169612d3bd92769d871edcdb9ab21d495b5
medinatrum <84572295+medinatrum@users.noreply.github.com>
2021-09-19 17:06:05 +0200

    Create main.tf

---
+++ terraform-modules/stage/data-stores/mysql/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+ region = "us-east-2"                   
+}
+
+resource "aws_db_instance" "example" {
+ identifier_prefix = "zavrsni-rad"                            
+ engine = "mysql"
+ allocated_storage = 10
+ instance_class = "db.t2.micro"
+ name = var.db_name                 
+ username = "medina"      
+                               
+ skip_final_snapshot = true    
+ password = var.db_password
+}
+
+terraform {
+ backend "s3" {
+ bucket = "diplomski-rad-terraform-state"                                   
+ key = "stage/data-stores/mysql/terraform.tfstate"
+ region = "us-east-2"
+ dynamodb_table = "terraform-locks"                            
+ encrypt = true
+ }
+}
